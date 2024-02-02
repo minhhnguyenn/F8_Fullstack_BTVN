c02b936c479612cae042a21ea7f5630cf8805e6d
unknown <minh.ngnn@gmail.com>
2024-02-02 17:13:09 +0700

    Btvn buổi 20

--- Btvnbuoi20/js/Btvnbuoi20.js
+++ Btvnbuoi20/js/Btvnbuoi20.js
@@ -1,9 +1,12 @@
 var text = document.getElementById("text");
-var string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis soluta incidunt quo quis, odio aut 1500s ";
+var string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis soluta incidunt quo quis, odio aut 1500s";
 var newString;
 var i = 0;
 function addColor(string) {
     var subString = string.slice(i, string.indexOf(" ", (i + 1)))
+    if (string.indexOf(" ", (i + 1)) === -1) {
+        subString = string.slice(i, (string.length));
+    }
     newString = string.replace(subString, `<span style="color: red;">${subString}</span>`);
     text.innerHTML = `${newString}`;
     i = string.indexOf(" ", (i + 1));
