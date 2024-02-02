0e650ff1393d5a2d51b6f619749b3863bbda5e4d
unknown <minh.ngnn@gmail.com>
2024-02-02 15:42:44 +0700

    nộp bài tập buổi 20

---
+++ Btvnbuoi20/js/Btvnbuoi20.js
@@ -0,0 +1,14 @@
+var text = document.getElementById("text");
+var string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis soluta incidunt quo quis, odio aut 1500s ";
+var newString;
+var i = 0;
+function addColor(string) {
+    var subString = string.slice(i, string.indexOf(" ", (i + 1)))
+    newString = string.replace(subString, `<span style="color: red;">${subString}</span>`);
+    text.innerHTML = `${newString}`;
+    i = string.indexOf(" ", (i + 1));
+    if (i === -1) {
+        i = 0
+    }
+}
+setInterval(function () { addColor(string) }, 500)
